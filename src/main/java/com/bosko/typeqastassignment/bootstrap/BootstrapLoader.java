ecabbc156c67d2924207795c0a2a2daef0556dff
boskodjokic <boskodjokic@yahoo.com>
2021-05-26 17:12:32 +0200

    Refactored bootstrap loader.

--- src/main/java/com/bosko/typeqastassignment/bootstrap/BootstrapLoader.java
+++ src/main/java/com/bosko/typeqastassignment/bootstrap/BootstrapLoader.java
@@ -3,25 +3,36 @@ package com.bosko.typeqastassignment.bootstrap;
 import com.bosko.typeqastassignment.entity.Address;
 import com.bosko.typeqastassignment.entity.Client;
 import com.bosko.typeqastassignment.entity.Meter;
+import com.bosko.typeqastassignment.entity.Reading;
 import com.bosko.typeqastassignment.repository.AddressRepository;
 import com.bosko.typeqastassignment.repository.ClientRepository;
+import com.bosko.typeqastassignment.repository.MeterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class BootstrapLoader implements CommandLineRunner {
 
+    @Autowired
     private ClientRepository clientRepository;
+    @Autowired
     private AddressRepository addressRepository;
+    @Autowired
+    private MeterRepository meterRepository;
 
-    public BootstrapLoader(ClientRepository clientRepository, AddressRepository addressRepository) {
-        this.clientRepository = clientRepository;
-        this.addressRepository = addressRepository;
-    }
+
+//    public BootstrapLoader(ClientRepository clientRepository, AddressRepository addressRepository) {
+//        this.clientRepository = clientRepository;
+//        this.addressRepository = addressRepository;
+//    }
 
     @Override
     public void run(String... args) throws Exception {
-//        loadAddresses();
         loadClients();
     }
 
@@ -36,7 +47,13 @@ public class BootstrapLoader implements CommandLineRunner {
         address.setStreet("Dragoslava Srejovica");
         addressRepository.save(address);
         client1.setAddress(address);
-        client1.setMeter(new Meter());
+        Meter meter = new Meter();
+        List<Reading> readings = new ArrayList<>();
+        readings.add(new Reading(12, LocalDate.of(2021, 12, 10), meter));
+        readings.add(new Reading(35, LocalDate.of(2021, 11, 15), meter));
+        meter.setReadings(readings);
+        meterRepository.save(meter);
+        client1.setMeter(meter);
         clientRepository.save(client1);
 
 
@@ -45,23 +62,19 @@ public class BootstrapLoader implements CommandLineRunner {
         client2.setFirstName("Ivana");
         client2.setLastName("Djokic");
         client2.setMeter(new Meter());
-
-        clientRepository.save(client2);
-    }
-
-    public void loadAddresses() {
-        Address address1 = new Address();
-        address1.setId(1L);
-        address1.setCity("Beograd");
-        address1.setStreet("Dragoslava Srejovica");
-        address1.setNumber("74");
-        addressRepository.save(address1);
-
         Address address2 = new Address();
-        address2.setId(2L);
-        address2.setCity("Beograd");
-        address2.setStreet("Dragoslava Srejovica");
-        address2.setNumber("74");
+        address2.setCity("Loznica");
+        address2.setNumber("15");
+        address2.setStreet("Vuka Karadzica");
         addressRepository.save(address2);
+        client2.setAddress(address2);
+        Meter meter2 = new Meter();
+        List<Reading> readings2 = new ArrayList<>();
+        readings2.add(new Reading(12, LocalDate.of(2021, 10, 1), meter2));
+        readings2.add(new Reading(25, LocalDate.of(2021, 7, 12), meter2));
+        meter2.setReadings(readings2);
+        meterRepository.save(meter2);
+        client2.setMeter(meter2);
+        clientRepository.save(client2);
     }
 }
