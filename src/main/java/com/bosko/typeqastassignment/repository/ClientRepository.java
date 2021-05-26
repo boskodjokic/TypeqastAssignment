bacbf0471dbefb8f5515200b824556987649bee9
boskodjokic <boskodjokic@yahoo.com>
2021-05-26 17:06:46 +0200

    Adding queries to reading repository.

--- src/main/java/com/bosko/typeqastassignment/repository/ClientRepository.java
+++ src/main/java/com/bosko/typeqastassignment/repository/ClientRepository.java
@@ -4,12 +4,13 @@ import com.bosko.typeqastassignment.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    Optional<Client> findByLastName(String lastName);
+    List<Client> findByLastName(String lastName);
 
     Optional<Client> findByFirstNameAndLastName(String firstName, String lastName);
 }
--- src/main/java/com/bosko/typeqastassignment/repository/ReadingRepository.java
+++ src/main/java/com/bosko/typeqastassignment/repository/ReadingRepository.java
@@ -1,9 +1,28 @@
 package com.bosko.typeqastassignment.repository;
 
+import com.bosko.typeqastassignment.dto.UserReading;
 import com.bosko.typeqastassignment.entity.Reading;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReadingRepository extends JpaRepository<Reading, Long> {
+
+    @Query(value = "SELECT reading.create_date, reading.value, reading.meter_id, client.first_name, client.last_name\n" +
+            "FROM reading\n" +
+            "LEFT JOIN meter ON reading.meter_id = meter.id\n" +
+            "LEFT JOIN client ON client.meter_id = meter.id WHERE client.id = :id", nativeQuery = true)
+    List<UserReading> getAllReadingsForClientId(@Param("id") Long id);
+
+    @Query(value = "SELECT YEAR(reading.create_date), reading.meter_id, client.first_name, client.last_name, SUM(reading.value) as value\n" +
+            "FROM reading\n" +
+            "LEFT JOIN meter ON reading.meter_id = meter.id\n" +
+            "LEFT JOIN client ON client.meter_id = meter.id WHERE client.id = :id\n" +
+            "GROUP BY YEAR(reading.create_date)", nativeQuery = true)
+    Optional<UserReading> getTotalValueOfReadingsForClientId(@Param("id") Long id);
 }
