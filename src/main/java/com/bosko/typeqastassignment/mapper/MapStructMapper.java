1e4fb8ea45ec07acc77900a17124c20140665815
boskodjokic <boskodjokic@yahoo.com>
2021-05-27 22:14:03 +0200

    Refactored MapStructMapper.

--- src/main/java/com/bosko/typeqastassignment/mapper/MapStructMapper.java
+++ src/main/java/com/bosko/typeqastassignment/mapper/MapStructMapper.java
@@ -6,10 +6,9 @@ import com.bosko.typeqastassignment.entity.Client;
 import com.bosko.typeqastassignment.entity.Meter;
 import com.bosko.typeqastassignment.entity.Reading;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface MapStructMapper {
 
     MapStructMapper INSTANCE = Mappers.getMapper(MapStructMapper.class);
@@ -30,9 +29,4 @@ public interface MapStructMapper {
 
     Address transformAddressDTOToEntity(AddressDTO addressDTO);
 
-    @Mapping(source = "first_name", target = "firstName")
-    @Mapping(source = "last_name", target = "lastName")
-    @Mapping(source = "create_date", target = "createDate", dateFormat = "dd.MM.yyyy")
-    @Mapping(source = "meter_id", target = "meterId")
-    UserReadingDTO transformUserReadingToDTO(UserReading userReading);
 }
