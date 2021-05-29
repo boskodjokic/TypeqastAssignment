02e125b7f6f747de7e1f0b490277d14a76022399
boskodjokic <boskodjokic@yahoo.com>
2021-05-29 22:37:11 +0200

    Added SwaggerConfig and annotations for controllers.

--- src/main/java/com/bosko/typeqastassignment/controller/v1/ClientController.java
+++ src/main/java/com/bosko/typeqastassignment/controller/v1/ClientController.java
@@ -2,12 +2,17 @@ package com.bosko.typeqastassignment.controller.v1;
 
 import com.bosko.typeqastassignment.api.v1.dto.ClientDTO;
 import com.bosko.typeqastassignment.service.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Client Controller")
+@Tag(name = "Client Controller", description = "This is client controller")
 @RestController
 @RequestMapping(ClientController.BASE_URL)
 public class ClientController {
@@ -17,42 +22,49 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @ApiOperation(value = "This will return list of all clients")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> getAllClients() {
         return clientService.getAllClients();
     }
 
+    @ApiOperation(value = "This will return a client with specified ID")
     @GetMapping("/{clientId}")
     @ResponseStatus(HttpStatus.OK)
     public ClientDTO getClientById(@PathVariable Long clientId) {
         return clientService.getClientById(clientId);
     }
 
+    @ApiOperation(value = "This will create new client")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ClientDTO createNewClient(@RequestBody ClientDTO clientDTO) throws Exception {
         return clientService.createNewClient(clientDTO);
     }
 
+    @ApiOperation(value = "This will update existing client")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ClientDTO updateClient(@PathVariable Long id, @RequestBody ClientDTO clientDTO) {
         return clientService.updateClient(id, clientDTO);
     }
 
+    @ApiOperation(value = "This will delete existing client")
     @DeleteMapping("/{clientId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteClient(@PathVariable Long clientId) {
         clientService.deleteClientById(clientId);
     }
 
+    @ApiOperation(value = "This will return a client or a list of clients with specified last name")
     @GetMapping("get/{lastName}")
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> findByLastName(@PathVariable("lastName") String lastName) {
         return clientService.findByLastName(lastName);
     }
 
+    @ApiOperation(value = "This will return a client or a list of clients with specified first and last name")
     @GetMapping("/{firstName}/{lastName}")
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> findByFirstNameAndLastName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
--- src/main/java/com/bosko/typeqastassignment/controller/v1/ReadingController.java
+++ src/main/java/com/bosko/typeqastassignment/controller/v1/ReadingController.java
@@ -3,12 +3,17 @@ package com.bosko.typeqastassignment.controller.v1;
 import com.bosko.typeqastassignment.api.v1.dto.ReadingDTO;
 import com.bosko.typeqastassignment.exceptions.ResourceNotFoundException;
 import com.bosko.typeqastassignment.service.ReadingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Reading Controller")
+@Tag(name = "Reading Controller", description = "This is reading controller")
 @RestController
 @RequestMapping(ReadingController.BASE_URL)
 public class ReadingController {
@@ -18,48 +23,56 @@ public class ReadingController {
     @Autowired
     private ReadingService readingService;
 
+    @ApiOperation(value = "This will return error, because no ID of a client is specified")
     @GetMapping
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void incompleteData() {
         throw new ResourceNotFoundException();
     }
 
+    @ApiOperation(value = "This will return list of all reading for specified client ID")
     @GetMapping("{clientId}")
     @ResponseStatus(HttpStatus.OK)
     public List<ReadingDTO> getReadingsByClientId(@PathVariable Long clientId) {
         return readingService.getAllReadingsForClientId(clientId);
     }
 
+    @ApiOperation(value = "This will return list of all reading for specified client ID")
     @GetMapping("/total/{clientId}/{year}")
     @ResponseStatus(HttpStatus.OK)
     public ReadingDTO getTotalReadingsByClientId(@PathVariable Long clientId, @PathVariable int year) {
         return readingService.getTotalOfReadingsForClientId(clientId, year);
     }
 
+    @ApiOperation(value = "This will return total value of reading for specified client ID and year")
     @GetMapping("{clientId}/{year}")
     @ResponseStatus(HttpStatus.OK)
     public List<ReadingDTO> getTotalReadingsByClientIdPerYear(@PathVariable Long clientId, @PathVariable int year) {
         return readingService.getAllReadingsPerClientPerYear(clientId, year);
     }
 
+    @ApiOperation(value = "This will return a reading for specified client ID, month and year")
     @GetMapping("{clientId}/{month}/{year}")
     @ResponseStatus(HttpStatus.OK)
     public ReadingDTO getReadingForClientIdForYearAndMonth(@PathVariable Long clientId, @PathVariable String month, @PathVariable int year) {
         return readingService.getReadingForClientIdForMonth(clientId, month, year);
     }
 
+    @ApiOperation(value = "This will create new reading for specified client ID")
     @PostMapping("{clientId}")
     @ResponseStatus(HttpStatus.CREATED)
     public ReadingDTO createNewReading(@PathVariable Long clientId, @RequestBody ReadingDTO readingDTO) {
         return readingService.createNewReading(clientId, readingDTO);
     }
 
+    @ApiOperation(value = "This will update existing reading for specified client ID")
     @PutMapping("{clientId}")
     @ResponseStatus(HttpStatus.OK)
     public ReadingDTO updateReading(@PathVariable Long clientId, @RequestBody ReadingDTO readingDTO) {
         return readingService.updateReading(clientId, readingDTO);
     }
 
+    @ApiOperation(value = "This will delete a reading for specified client ID, month and year")
     @DeleteMapping("{clientId}/{month}/{year}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteReading(@PathVariable Long clientId, @PathVariable String month, @PathVariable int year) {
