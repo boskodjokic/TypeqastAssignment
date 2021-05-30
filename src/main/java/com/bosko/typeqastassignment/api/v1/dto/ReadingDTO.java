ba7c3b551a599d86247fac502adfe7825fca9c0b
boskodjokic <boskodjokic@yahoo.com>
2021-05-30 21:08:23 +0200

    Added annotations for swagger in ReadingDTO.

--- src/main/java/com/bosko/typeqastassignment/api/v1/dto/ReadingDTO.java
+++ src/main/java/com/bosko/typeqastassignment/api/v1/dto/ReadingDTO.java
@@ -1,20 +1,35 @@
 package com.bosko.typeqastassignment.api.v1.dto;
 
 import com.bosko.typeqastassignment.entity.Meter;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+import javax.persistence.Column;
+
+@ApiModel(description = "Details about the readings")
 @Data
 public class ReadingDTO {
 
-    @JsonBackReference(value = "readingId")
+    @ApiModelProperty(hidden = true, name = "Reading ID", value = "1")
+    @JsonIgnore
     private Long id;
 
+    @ApiModelProperty(name = "Value", value = "25")
+    @Column(nullable = false, length = 4)
     private int value;
+
+    @ApiModelProperty(name = "Month", value = "May")
+    @Column(nullable = false, length = 100)
     private String month;
+
+    @ApiModelProperty(name = "Year", value = "2021")
+    @Column(nullable = false, length = 4)
     private String year;
 
-    @JsonBackReference(value = "meter")
+    @JsonIgnore
+    @ApiModelProperty(hidden = true)
     private Meter meter;
 
 }
--- src/main/java/com/bosko/typeqastassignment/controller/v1/ClientController.java
+++ src/main/java/com/bosko/typeqastassignment/controller/v1/ClientController.java
@@ -4,6 +4,7 @@ import com.bosko.typeqastassignment.api.v1.dto.ClientDTO;
 import com.bosko.typeqastassignment.service.ClientService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,49 +23,56 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
-    @ApiOperation(value = "This will return list of all clients")
+    @ApiOperation(value = "Returns a list of all clients",
+            notes = "Just execute GET method on BASE_URL for clients (/api/v1/clients)")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> getAllClients() {
         return clientService.getAllClients();
     }
 
-    @ApiOperation(value = "This will return a client with specified ID")
+    @ApiOperation(value = "Returns a client with specified ID",
+            notes = "Provide an ID to look for a specific client")
     @GetMapping("/{clientId}")
     @ResponseStatus(HttpStatus.OK)
-    public ClientDTO getClientById(@PathVariable Long clientId) {
+    public ClientDTO getClientById(@ApiParam(value = "ID value for the client that you need to be retrieved", required = true) @PathVariable Long clientId) {
         return clientService.getClientById(clientId);
     }
 
-    @ApiOperation(value = "This will create new client")
+    @ApiOperation(value = "This will create new client",
+            notes = "Input first name, last name, and full address of new client(street, number, city)")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ClientDTO createNewClient(@RequestBody ClientDTO clientDTO) throws Exception {
         return clientService.createNewClient(clientDTO);
     }
 
-    @ApiOperation(value = "This will update existing client")
+    @ApiOperation(value = "This will update existing client",
+            notes = "Provide id of client that needs to be updated and input first or last name that should be changed")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ClientDTO updateClient(@PathVariable Long id, @RequestBody ClientDTO clientDTO) {
         return clientService.updateClient(id, clientDTO);
     }
 
-    @ApiOperation(value = "This will delete existing client")
+    @ApiOperation(value = "This will delete existing client",
+            notes = "Provide an ID to delete a specific client")
     @DeleteMapping("/{clientId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteClient(@PathVariable Long clientId) {
         clientService.deleteClientById(clientId);
     }
 
-    @ApiOperation(value = "This will return a client or a list of clients with specified last name")
+    @ApiOperation(value = "This will return a client or a list of clients with specified last name",
+            notes = "Provide a last name to look for a specific client or clients")
     @GetMapping("get/{lastName}")
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> findByLastName(@PathVariable("lastName") String lastName) {
         return clientService.findByLastName(lastName);
     }
 
-    @ApiOperation(value = "This will return a client or a list of clients with specified first and last name")
+    @ApiOperation(value = "This will return a client or a list of clients with specified first and last name",
+            notes = "Provide a first name and last name to look for a specific client or clients")
     @GetMapping("/{firstName}/{lastName}")
     @ResponseStatus(HttpStatus.OK)
     public List<ClientDTO> findByFirstNameAndLastName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
